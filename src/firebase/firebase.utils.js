373043f36afb2f57cde724bc8c560225c0d26829
AndresFSalcedo <andres.salcedo95@hotmail.com>
2022-02-19 12:43:21 +1300

    Integrated firebase
    Create Sig In page with google authentication.

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,25 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDXWwjO6APv8ufZrD5jJq8X0aykWOmqN2c",
+    authDomain: "crwn-db-61f81.firebaseapp.com",
+    projectId: "crwn-db-61f81",
+    storageBucket: "crwn-db-61f81.appspot.com",
+    messagingSenderId: "256360592765",
+    appId: "1:256360592765:web:406fffc623d3002a118814",
+    measurementId: "G-T71W5LR573"
+  };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({prompt: 'select_account'});
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
