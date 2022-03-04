da6290e2c5be6893f73f5ee9ddfae685d2b3b777
AndresFSalcedo <andres.salcedo95@hotmail.com>
2022-03-04 18:28:19 +1300

    Add Redux
    Implemented user reducer and actions to header and app component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import './App.css';
 
@@ -8,38 +9,32 @@ import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import {auth, createUserProfileDocument} from './firebase/firebase.utils';
-
+import {setCurrentUSer} from './redux/user/user.actions';
 
 class App extends React.Component {
-  constructor(){
-    super();
-
-    this.state ={
-      currentUser: null
-    }
-  }
- 
+   
   unsubscribeFromAuth = null;
 
   componentDidMount(){
+
+    const {setCurrentUSer} = this.props;
+
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
 
       if(userAuth){
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapshot =>{
-          this.setState({
-            currentUser: {
-              id: snapshot.id,
-              ...snapshot.data()
-            }
+          setCurrentUSer({
+            id: snapshot.id,
+            ...snapshot.data()
           });
           console.log(this.state)
         });
         
       }
       else {
-        this.setState({currentUser: userAuth});
+        setCurrentUSer(userAuth);
       }
     })
   }
@@ -51,7 +46,7 @@ class App extends React.Component {
   render(){
     return (
     <div>
-      <Header currentUser={this.state.currentUser}/>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route exact path='/shop' component={ShopPage}/>
@@ -64,4 +59,8 @@ class App extends React.Component {
   
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUSer: user => dispatch(setCurrentUSer(user))
+})
+
+export default connect(null, mapDispatchToProps)(App);
--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -30,7 +30,10 @@ class SignUp extends React.Component{
         }
 
         try{
-            const {user} = await auth.createUserWithEmailAndPassword(email, password);
+            const {user} = await auth.createUserWithEmailAndPassword(
+                email,
+                password
+            );
 
             await createUserProfileDocument(user, {displayName});
 
@@ -104,7 +107,5 @@ class SignUp extends React.Component{
         )
     }
 }
-
-
 
 export default SignUp;
