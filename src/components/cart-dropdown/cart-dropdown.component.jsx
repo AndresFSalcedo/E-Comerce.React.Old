151ff9034867f40bec91890dbd417a5e3e3e2dbe
AndresFSalcedo <andres.salcedo95@hotmail.com>
2022-03-13 22:36:36 +1300

    Revert "Update"
    This reverts commit 08e74f0d4566aa866b465df0f677bff44dba9e2e.

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,29 +1,27 @@
 import React from 'react';
 
-<<<<<<< HEAD
 import { connect } from 'react-redux';
 
 import { selectCartItems } from '../../redux/cart/cart.selectors';
 import CartItem from '../cart-item/cart-item.component';
-=======
->>>>>>> parent of ccf990c (Create Cart-item component)
 import CustomButton from '../custom-button/custom-button.component';
 
 import './cart-dropdown.style.scss';
 
-const CartDropdown = () => (
+const CartDropdown = ({cartItems}) => (
     <div className='cart-dropdown'>
-        <div className="cart-items"></div>
+        <div className="cart-items">
+            {
+                cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem}/>)
+            }
+        </div>
+
         <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
 )
 
-<<<<<<< HEAD
 const mapStateToProps = (state) => ({
     cartItems: selectCartItems(state)
 })
 
 export default connect(mapStateToProps)(CartDropdown);
-=======
-export default CartDropdown;
->>>>>>> parent of ccf990c (Create Cart-item component)
