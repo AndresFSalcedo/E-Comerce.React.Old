a3a24457109cfa4fb68a1b65d502eacffc8842bd
AndresFSalcedo <andres.salcedo95@hotmail.com>
2022-03-13 22:36:18 +1300

    Revert "Update"
    This reverts commit c41b7673e9433536d23d742e2f69de537866be56.

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
+<<<<<<< HEAD
 import { connect } from 'react-redux';
 
 import { selectCartItems } from '../../redux/cart/cart.selectors';
-//import CartItem from '../cart-item/cart-item.component';
+import CartItem from '../cart-item/cart-item.component';
+=======
+>>>>>>> parent of ccf990c (Create Cart-item component)
 import CustomButton from '../custom-button/custom-button.component';
 
 import './cart-dropdown.style.scss';
@@ -15,8 +18,12 @@ const CartDropdown = () => (
     </div>
 )
 
+<<<<<<< HEAD
 const mapStateToProps = (state) => ({
     cartItems: selectCartItems(state)
 })
 
 export default connect(mapStateToProps)(CartDropdown);
+=======
+export default CartDropdown;
+>>>>>>> parent of ccf990c (Create Cart-item component)
