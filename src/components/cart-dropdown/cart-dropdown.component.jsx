c41b7673e9433536d23d742e2f69de537866be56
AndresFSalcedo <andres.salcedo95@hotmail.com>
2022-03-13 22:35:44 +1300

    Update

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
 
-<<<<<<< HEAD
 import { connect } from 'react-redux';
 
 import { selectCartItems } from '../../redux/cart/cart.selectors';
-import CartItem from '../cart-item/cart-item.component';
-=======
->>>>>>> parent of ccf990c (Create Cart-item component)
+//import CartItem from '../cart-item/cart-item.component';
 import CustomButton from '../custom-button/custom-button.component';
 
 import './cart-dropdown.style.scss';
@@ -18,12 +15,8 @@ const CartDropdown = () => (
     </div>
 )
 
-<<<<<<< HEAD
 const mapStateToProps = (state) => ({
     cartItems: selectCartItems(state)
 })
 
 export default connect(mapStateToProps)(CartDropdown);
-=======
-export default CartDropdown;
->>>>>>> parent of ccf990c (Create Cart-item component)
